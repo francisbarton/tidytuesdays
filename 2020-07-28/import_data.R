24f2afb60598b52489d0f4c91c86415f7a04d623
Francis Barton <fbarton@alwaysdata.net>
2020-07-29 11:06:23 +0100

    initial upload

--- 2020-07-28/import_data.R
+++ 2020-07-28/import_data.R
@@ -1,9 +1,10 @@
-library(here)
 
 
-penguins.csv <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-28/penguins.csv'
+# myrmidon::download_file(penguins.csv)
+# myrmidon::download_file(penguins_raw.csv)
 
-penguins_raw.csv <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-28/penguins_raw.csv'
 
-download_file(penguins.csv, dir = latest)
-download_file(penguins_raw.csv, dir = latest)
+penguins <- readr::read_csv("penguins.csv")
+penguins_raw <- readr::read_csv("penguins_raw.csv")
+
+
